93de838278aa839dfa82926966d28f15b2d92a25
nikita <notgurev@gmail.com>
2022-05-27 23:04:55 +0300

    init

---
+++ src/main/kotlin/foxgurev/blps/lab4/BlpsLab4Application.kt
@@ -0,0 +1,11 @@
+package foxgurev.blps.lab4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlpsLab4Application
+
+fun main(args: Array<String>) {
+    runApplication<BlpsLab4Application>(*args)
+}
